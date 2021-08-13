27de80cd9a71617dcd61abf2862ea132b0ca8c6c
pradip <dhpradeep25@gmail.com>
2021-08-13 13:16:24 +0545

    fix indentation

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,76 +3,71 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  organizationName: "eversoft",
-  projectName: "coding-standard",
-  url: "http://eversoft-group.github.io",
-  baseUrl: "/coding-standard/",
-  trailingSlash: false,
-  title: "Eversoft Coding Standard",
-  tagline: "We are cool",
-  url: "https://eversoftgroup.com",
-  baseUrl: "https://eversoft-group.github.io/coding-standard/",
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
-  favicon: "img/favicon.ico",
-  organizationName: "Eversoft", // Usually your GitHub org/user name.
-  projectName: "coding standard", // Usually your repo name.
+  title: 'Eversoft Coding Standard',
+  tagline: 'We are cool',
+  url: 'https://eversoft-group.github.io',
+  baseUrl: '/coding-standard/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'Eversoft', // Usually your GitHub org/user name.
+  projectName: 'coding-standard', // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: "Eversoft Coding Standard",
+      title: 'Eversoft Coding Standard',
       logo: {
-        alt: "Eversoft Logo",
-        src: "https://eversoftgroup.com/wp-content/uploads/2020/10/png-logo-of-eversoft-group.png",
+        alt: 'Eversoft Logo',
+        src: 'https://eversoftgroup.com/wp-content/uploads/2020/10/png-logo-of-eversoft-group.png',
       },
       items: [
         {
-          type: "doc",
-          docId: "intro",
-          position: "left",
-          label: "Docs",
+          type: 'doc',
+          docId: 'intro',
+          position: 'left',
+          label: 'Docs',
         },
         {
-          href: "https://github.com/facebook/docusaurus",
-          label: "GitHub",
-          position: "right",
+          href: 'https://github.com/facebook/docusaurus',
+          label: 'GitHub',
+          position: 'right',
         },
       ],
     },
     footer: {
-      style: "dark",
+      style: 'dark',
       links: [
         {
-          title: "Docs",
+          title: 'Docs',
           items: [
             {
-              label: "Coding Standard",
-              to: "/docs/intro",
+              label: 'Coding Standard',
+              to: '/docs/intro',
             },
           ],
         },
         {
-          title: "Community",
+          title: 'Community',
           items: [
             {
-              label: "Facebook",
-              href: "https://stackoverflow.com/questions/tagged/docusaurus",
+              label: 'Facebook',
+              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
             },
             {
-              label: "Linkedln",
-              href: "https://discordapp.com/invite/docusaurus",
+              label: 'Linkedln',
+              href: 'https://discordapp.com/invite/docusaurus',
             },
           ],
         },
         {
-          title: "More",
+          title: 'More',
           items: [
             {
-              label: "Blog",
-              to: "https://eversoftgroup.com/blog/",
+              label: 'Blog',
+              to: 'https://eversoftgroup.com/blog/',
             },
             {
-              label: "GitHub",
-              href: "https://github.com/facebook/docusaurus",
+              label: 'GitHub',
+              href: 'https://github.com/facebook/docusaurus',
             },
           ],
         },
@@ -86,22 +81,22 @@ module.exports = {
   },
   presets: [
     [
-      "@docusaurus/preset-classic",
+      '@docusaurus/preset-classic',
       {
         docs: {
-          sidebarPath: require.resolve("./sidebars.js"),
+          sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            "https://github.com/facebook/docusaurus/edit/master/website/",
+            'https://github.com/facebook/docusaurus/edit/master/website/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            "https://github.com/facebook/docusaurus/edit/master/website/blog/",
+            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
         },
         theme: {
-          customCss: require.resolve("./src/css/custom.css"),
+          customCss: require.resolve('./src/css/custom.css'),
         },
       },
     ],
